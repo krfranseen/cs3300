42c0b21c8724757e4e99246463b4f26872957e64
krfranseen <kfransee@uccs.edu>
2021-05-07 05:51:34 +0000

    Modified Navbar to match specs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects or /projects.json
   def index
     @projects = Project.all
+    @student = Student.find_by_id(1)
   end
 
   # GET /projects/1 or /projects/1.json
